fad1004f2a9b8e68ec71d653d5e8f4aac546f52f
Jouusey <josevitor110m.p@gmail.com>
2025-08-20 17:11:52 -0300

    Branch da pagina de customização criado.

--- src/paginas/PaginaCustomizaçao.jsx
+++ src/paginas/PaginaCustomizaçao.jsx
@@ -7,7 +7,7 @@ function PaginaCustomizaçao() {
     <div className="container-pagina">
       <div className="container">
 
-        <h1 className='logo-texto' >FORJA</h1>
+        <h1 className='logo-texto' > FORJA </h1>
         <h2 className='logo-embaixo-texto' >CRIAÇÃO DE PERSONAGENS PARA RPG</h2>
 
         <button className='btn-entrar' >ENTRAR</button>
