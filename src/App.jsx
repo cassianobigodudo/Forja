2dd6343114e36ffa68c2d95931ed74d70a060e00
Jouusey <josevitor110m.p@gmail.com>
2025-08-20 17:03:59 -0300

    REACT e NPM instalados, fontes, icones e fundo agora estão no projeto. Pastas para paginas, fontes, personagem e mais adicionadas.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Teste from './paginas/Teste'
+import PaginaCustomizaçao from './paginas/PaginaCustomizaçao'
+
+function App() {
+
+  return (
+    
+    <Teste/>,
+    <PaginaCustomizaçao/>
+      
+  )
+}
+
+export default App
---
+++ src/paginas/PaginaCustomizaçao.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./PaginaCustomizaçao.css"
+
+function PaginaCustomizaçao() {
+  return (
+
+    <div className="container-pagina">
+      <div className="container">
+
+        <h1 className='logo-texto' >FORJA</h1>
+        <h2 className='logo-embaixo-texto' >CRIAÇÃO DE PERSONAGENS PARA RPG</h2>
+
+        <button className='btn-entrar' >ENTRAR</button>
+
+
+      </div>
+    </div>
+
+  )
+}
+
+export default PaginaCustomizaçao
