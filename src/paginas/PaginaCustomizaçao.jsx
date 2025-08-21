f8bdee0e21b38971b04276bde1c2d74a136af8ae
Jouusey <josevitor110m.p@gmail.com>
2025-08-21 13:56:19 -0300

    teste. (sem mudanças)

--- src/paginas/PaginaCustomizaçao.jsx
+++ src/paginas/PaginaCustomizaçao.jsx
@@ -10,7 +10,7 @@ function PaginaCustomizaçao() {
         <h1 className='logo-texto' > FORJA </h1>
         <h2 className='logo-embaixo-texto' >CRIAÇÃO DE PERSONAGENS PARA RPG</h2>
 
-        <button className='btn-entrar' >ENTRAR</button>
+        <button className='btn-entrar' >ENTRAR!</button>
 
 
       </div>
