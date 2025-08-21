e6e45dfa8e74b868444c2e023d17756773e3d911
Jouusey <josevitor110m.p@gmail.com>
2025-08-21 16:58:50 -0300

    Progresso na pagina de customização.

--- src/paginas/PaginaCustomizaçao.jsx
+++ src/paginas/PaginaCustomizaçao.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
 import "./PaginaCustomizaçao.css"
+import Navbar from '../componentes/Navbar'
 
 function PaginaCustomizaçao() {
   return (
 
     <div className="container-pagina">
-      <div className="container">
+      <div className="pagina-custom">
 
-        <h1 className='logo-texto' > FORJA </h1>
-        <h2 className='logo-embaixo-texto' >CRIAÇÃO DE PERSONAGENS PARA RPG</h2>
+        <div className="navbar-container">
+          <Navbar />
+        </div>
 
-        <button className='btn-entrar' >ENTRAR!</button>
+        <div className="area-customizacao">
+          <div className="paperdoll-area">
+            <img src="./personagem-FEMININO/CORPO-FEMININO.png" alt="" />
+          </div>
+          
+          <div className="menu-primario-custom-container">
 
+            <div className="menu-primario-custom-top">
+
+             <button className='btn-corpo'>CORPO  <img src="./icones/Corpo.svg" alt="" /></button>
+             <button className='btn-corpo-follow'>CABEÇA  <img src="./icones/Cabeça.svg" alt="" /></button>
+             <button className='btn-corpo-follow'>TORSO  <img src="./icones/Torso.svg" alt="" /></button>
+             <button className='btn-corpo-follow'>PERNA  <img src="./icones/Perna.svg" alt="" /></button>
+             <button className='btn-corpo-follow'>SAPATO  <img src="./icones/Pé.svg" alt="" /></button>
+             <button className='btn-corpo-follow'>BASE  <img src="./icones/Base.svg" alt="" /></button>
+             <button className='btn-corpo-follow'>HISTÓRIA  <img src="./icones/História.svg" alt="" /></button>
+            
+
+            </div>
+            
+            <div className="menu-primario-custom-bottom">
+
+              <button className='btn-zoom' ></button>
+              <button className='btn-tirar-zoom'></button>
+
+            </div>
+            
+          </div>
+          <div className="menu-secundario-custom"></div>
+
+
+        </div>
 
       </div>
     </div>
