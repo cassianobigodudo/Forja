4d06d39281e532fe86e5350b9484e44a60c9677a
cassianobigodudo <kakacassiano2010@hotmail.com>
2025-09-09 16:26:39 -0300

    implementação das rotas e do globalcontext

--- src/pages/PaginaCustomizaçao.jsx
+++ src/pages/PaginaCustomizaçao.jsx
@@ -1,22 +1,22 @@
-// import React from 'react'
-// import "./PaginaCustomizaçao.css"
+import React from 'react'
+import "./PaginaCustomizaçao.css"
 
-// function PaginaCustomizaçao() {
-//   return (
+function PaginaCustomizaçao() {
+  return (
 
-//     <div className="container-landing-page">
-//       <div className="container">
+    <div className="container-landing-page">
+      <div className="container">
 
-//         <h1 className='logo-texto' > FORJA </h1>
-//         <h2 className='logo-embaixo-texto' >CRIAÇÃO DE PERSONAGENS PARA RPG</h2>
+        <h1 className='logo-texto' > FORJA </h1>
+        <h2 className='logo-embaixo-texto' >CRIAÇÃO DE PERSONAGENS PARA RPG</h2>
 
-//         <button className='btn-entrar' >ENTRAR</button>
+        <button className='btn-entrar' >ENTRAR</button>
 
 
-//       </div>
-//     </div>
+      </div>
+    </div>
 
-//   )
-// }
+  )
+}
 
-// export default PaginaCustomizaçao
+export default PaginaCustomizaçao
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom"
 import LandingPage from "../pages/LandingPage"
+import UserAccount from "../pages/UserAccount"
+import PaginaCustomizaçao from "../pages/PaginaCustomizaçao"
 
 const router = createBrowserRouter([
 
     {path: "/", element: <LandingPage/>},
-    {path: ""}
+    {path: "/custom", element: <PaginaCustomizaçao/>},
+    {path: "/usuario", element: <UserAccount/>}
 
 ])
 
