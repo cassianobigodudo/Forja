763ada4fe4fa672002399b804d93f9ca44b69069
jaime-ac <jaimeantonioca97@gmail.com>
2025-09-11 16:59:18 -0300

    atualização do modal de detalhes

--- src/components/BoxItem.jsx
+++ src/components/BoxItem.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './BoxItem.css'
 
 function BoxItem() {
+  const [verDetalhes, setVerDetalhes] = useState(false);
   return (
     <div className='container-box-item'>
 
@@ -15,10 +16,45 @@ function BoxItem() {
         <label htmlFor="" className="label-dados-item">HORÁRIO:</label>
       </div>
       <div className="box-parte-detalhes">
-        <button className="botao-detalhes-item">Detalhes</button>
+        <button onClick={() => setVerDetalhes(true)} className="botao-detalhes-item">Detalhes</button>
       </div>
       
+      <dialog open={verDetalhes} className='dialog-ver-detalhes'>
+
+        <div className="container-dialog-detalhes">
+
+          <div className="dialog-detalhes-fechar">
+            <button onClick={() => setVerDetalhes(false)} className='dialog-botao-fechar'>❌</button>
+          </div>
+
+          <div className="dialog-detalhes-informacoes">
+
+            <div className="dialog-detalhes-informacoes-esquerda">
+
+              <div className="dialog-detalhes-informacoes-esquerda-foto">
+                <img src="./public/images/personagem.png" alt="" className="dialog-detalhes-informacoes-esquerta-foto-personagem" />
+              </div>
+
+            </div>
+
+            <div className="dialog-detalhes-informacoes-direita">
+              <div className="dialog-detalhes-informacoes-direita-dados"></div>
+            </div>
+
+          </div>
+
+          <div className="dialog-detalhes-progresso">
+            <label htmlFor="">Barra de Progresso aqui...</label>
+          </div>
+
+
+        </div>
+
+
+      </dialog>
+
     </div>
+
   )
 }
 
