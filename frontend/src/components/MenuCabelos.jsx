a04d9c536b7d4746df50b0a9854b11dd7a972d75
jouusey <josevitor110m.p@gmail.com>
2025-09-18 15:23:35 -0300

    começo do componente de cabelo

---
+++ frontend/src/components/MenuCabelos.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './MenuCabelos.css'
+function MenuCabelos() {
+  return (
+    <div className="menu-container-cabelos">
+
+        
+        <button className='btn-acessorios'> 
+        <img src="./icones/ICONE-CORPO-MASCULINO.png" alt="Ícone corpo masculino" />
+        </button> 
+      
+     
+
+    </div>
+  )
+}
+
+export default MenuCabelos
--- frontend/src/components/MenuSecCabeca.jsx
+++ frontend/src/components/MenuSecCabeca.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 import "./MenuSecCabeca.css"
+import MenuCabelos from './MenuCabelos'
+
 
 function MenuSecCabeca() {
+  const [btnAtivo, setBtnAtivo] = useState('')
+
+  function handleButtonClick() {
+
+    if (btnAtivo == true) {
+
+      btnAtivo = false
+      
+    } else {
+
+      btnAtivo = true
+    }
+    
+  }
+
+
   return (
       <div className="container-menuSec-cabeca">
       <div className="top-menuSecc">  
-        <button className='btn-acessorios'>
+
+        <button className={btnAtivo === 'CABELO' ? 'btn-cabelo-ativado' : 'btn-cabelo'} 
+        onClick={() => handleButtonClick('CABELO')}> CABELO
         <img src="./icones/ICONE-CORPO-MASCULINO.png" alt="Ícone corpo masculino" />
         </button>  
-        <button className='btn-acessorios'>
+
+        <button className={btnAtivo === 'ACESSORIOS' ? 'btn-acessorios-ativado' : 'btn-'} 
+        onClick={() => handleButtonClick('ACESSORIOS')}> ACESSORIOS
+
         <img src="./icones/ICONE-CORPO-MASCULINO.png" alt="Ícone corpo masculino" />
         </button>  
-        <button className='btn-acessorios'>
+
+        <button className='btn-acessorios'> MARCAS
         <img src="./icones/ICONE-CORPO-MASCULINO.png" alt="Ícone corpo masculino" />
         </button>
       </div>
 
       <div className="bottom">
+
+        <MenuCabelos/>
       </div>
     </div>
   )
