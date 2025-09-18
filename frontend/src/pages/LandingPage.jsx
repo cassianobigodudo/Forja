1f6d87b7037d44fc7b3d94647f64c6e60545e27e
jaime-ac <jaimeantonioca97@gmail.com>
2025-09-18 14:43:23 -0300

    merge da minha branch

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,6 @@
-import React, {useRef, useState} from 'react'
+import React, {useRef} from 'react'
 import "./LandingPage.css"
 import Navbar from '../components/Navbar'
-import BoxLoginCadastro from '../components/BoxLoginCadastro'
-
 function LandingPage() {
   const sectionBaixoRef = useRef(null)
   const sectionAltoRef = useRef(null)
@@ -13,34 +11,23 @@ function LandingPage() {
     sectionAltoRef.current?.scrollIntoView({ behavior: 'smooth'})
   }
 
-  const [abrirBox, setAbrirBox] = useState(false)
-
   return (
 
     <>
-
       <div className="container-landing-page-cima"  ref={sectionAltoRef}>
         <Navbar/>
 
         <div className="container-centro">
           <label className='titulo-central'>FORJA</label>
           <label className='subtitulo-central'>CRIAÇÃO DE PERSONAGENS DE RPG</label>
-          <button className='btn-entrar' onClick={() => setAbrirBox(true)}>ENTRAR</button>
+          <button className='btn-entrar'>ENTRAR</button>
         </div>
 
         <div className="container-saberMais" onClick={SaberMais}>
           <label className='lbl-saibaMais' onClick={SaberMais}>Quer saber mais? Morra!</label>
           <img src="/icones/flecha-pra-baixo.svg" className='flechinha' onClick={SaberMais} />
         </div>
-        <dialog className='dialog-login-cadastro' open={abrirBox}>
-          <div className="container-dialog-log">
-
-            <BoxLoginCadastro />
-
-          </div>
-        </dialog>
       </div>
-
       <div className="container-landing-page-baixo" ref={sectionBaixoRef}>
         <div className="container-saberMenos" onClick={SaberMenos}>
           <label className='lbl-saibaMenos' onClick={SaberMais}>Quer saber menos? Viva!</label>
@@ -48,13 +35,11 @@ function LandingPage() {
 
         </div>
 
-        <div className="container-pai-infos-forja">
-          <div className="container-infos-esquerda"></div>
-          <div className="container-gif-direita"></div>
-        </div>
+      <div className="container-pai-infos-forja">
+        <div className="container-infos-esquerda"></div>
+        <div className="container-gif-direita"></div>
       </div>
-    
-
+      </div>
     </>
 
   )
