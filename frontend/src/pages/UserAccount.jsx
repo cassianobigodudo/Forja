9c50456065ebb18e3b92d54e508ee503896116b0
jouusey <josevitor110m.p@gmail.comn>
2025-10-29 15:18:34 -0300

    Progresso.

--- frontend/src/pages/UserAccount.jsx
+++ frontend/src/pages/UserAccount.jsx
@@ -57,7 +57,10 @@ function UserAccount() {
                     </div>
 
                     <div className="menu-parte-sair">
-                        <button className="botao-deslogar" onClick={() => alert('Botão para deslogar!')}>Sair</button>
+                         
+                        <button className="botao-deslogar" onClick={() => alert('Tem certeza que deseja deslogar de sua conta?')}>Sair</button>
+                        
+
                     </div>
 
                 </div>
