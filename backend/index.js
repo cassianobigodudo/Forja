9908394824096d90ea9cf6a9f2d82c9c632e998d
jaime-ac <jaimeantonioca97@gmail.com>
2025-11-06 17:08:02 -0300

    atualizar de novo para ver...

--- backend/index.js
+++ backend/index.js
@@ -10,34 +10,34 @@ const mainRoutes = require('./src/routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
-const HOST = '0.0.0.0'; // Necessário para o Render
+// const HOST = '0.0.0.0'; // Necessário para o Render
 
 // --- CONFIGURAÇÃO DE CORS À PROVA DE RENDER ---
 
-// 1. Defina QUEM pode acessar sua API
-const whitelist = [
-  'http://localhost:3000'
-];
+// // 1. Defina QUEM pode acessar sua API
+// const whitelist = [
+//   'http://localhost:3000'
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Permite requisições da whitelist (e requisições sem 'origin', como do Postman)
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error(`Origem não permitida pela política de CORS: ${origin}`));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Permite requisições da whitelist (e requisições sem 'origin', como do Postman)
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error(`Origem não permitida pela política de CORS: ${origin}`));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
 
-// 2. ESSA LINHA É A MAIS IMPORTANTE: Lida com o "preflight" (OPTIONS)
-// Isso resolve o erro "Redirect is not allowed" do Render.
-app.options('*', cors(corsOptions)); 
+// // 2. ESSA LINHA É A MAIS IMPORTANTE: Lida com o "preflight" (OPTIONS)
+// // Isso resolve o erro "Redirect is not allowed" do Render.
+// app.options('*', cors(corsOptions)); 
 
-// 3. Aplica a configuração de CORS a todas as outras rotas
-app.use(cors(corsOptions));
+// // 3. Aplica a configuração de CORS a todas as outras rotas
+// app.use(cors(corsOptions));
 
 // --------------------------------------------------
 
@@ -49,10 +49,10 @@ app.use(express.json({ limit: '10mb' }));
 // o 'personagemRoutes', 'carrinhoRoutes', etc.
 app.use('/api', mainRoutes);
 
-// Rota de teste para ver se o servidor está vivo
-app.get('/', (req, res) => {
-  res.send('Servidor Forja está VIVO!');
-});
+// // Rota de teste para ver se o servidor está vivo
+// app.get('/', (req, res) => {
+//   res.send('Servidor Forja está VIVO!');
+// });
 
 // 6. Inicia o servidor
 app.listen(PORT, HOST, () => {
