d97a7c71fa6cc3dc606f3f0df019a3695a078179
cassianobigodudo <kakacassiano2010@hotmail.com>
2025-11-10 17:11:21 -0300

    Implementação a caixa e o botão de cadastro e login, falta arrumar o CSS do botão de continuar, a lógica do frontend e do backend, banco de dados

--- frontend/src/components/CaixaCadastroLogin.jsx
+++ frontend/src/components/CaixaCadastroLogin.jsx
@@ -1,8 +1,25 @@
-import React from 'react    '   
+import React from 'react'
+import './CaixaCadastroLogin.css'
 
 function CaixaCadastroLogin() {
   return (
-    <div className='container-caixa-cadastrologin'>CaixaCadastroLogin</div>
+    <div className="container-avo">
+      <div className='container-caixa-cadastrologin'>
+        <div className="container-entrarcadastrar">
+          <label  className='lbl-selecionada'>Entrar</label>
+          <label  className='lbl-nao-selecionada'>Cadastrar</label>
+        </div>
+        <div className="container-inputs-cadastrologin">
+          <label className='lbl-inputs'>Email</label>
+          <input className='input-cadastrologin' type="text" placeholder='E-mail' />
+          <label className='lbl-inputs'>Senha</label>
+          <input className='input-cadastrologin' type="password" placeholder='Senha' />
+        </div>
+      </div>
+
+      <button className='btn-confirmar'>CONFIRMAR</button>
+
+    </div>
   )
 }
 
