2874889b48cc669b089cb10c8909918d636cb161
cassianobigodudo <kakacassiano2010@hotmail.com>
2025-11-19 16:44:36 -0300

    terminando a landing page

--- frontend/src/components/CaixaCadastroLogin.jsx
+++ frontend/src/components/CaixaCadastroLogin.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CaixaCadastroLogin.css'
 
 function CaixaCadastroLogin() {
+
+  const [isLogin, setIsLogin] = useState(true)
+  
+
   return (
     <div className="container-avo">
-      <div className='container-caixa-cadastrologin'>
+      <div className={isLogin ? "container-caixa-login" : "container-caixa-cadastro"}>
         <div className="container-entrarcadastrar">
-          <label  className='lbl-selecionada'>Entrar</label>
-          <label  className='lbl-nao-selecionada'>Cadastrar</label>
+          <label  className='lbl-selecionada'  >{isLogin ? 'Entrar' : 'Cadastrar'}</label>
+          <label  className='lbl-nao-selecionada' onClick={() => {
+            if (isLogin == true){setIsLogin(false)} else{setIsLogin(true)}}} >{isLogin ? 'Cadastrar' : 'Entrar'}</label>
         </div>
         <div className="container-inputs-cadastrologin">
+          {!isLogin && <label className='lbl-inputs'>Nickname</label>}
+          {!isLogin && <input className='input-cadastrologin' type="text" placeholder='Nickname' />}
           <label className='lbl-inputs'>Email</label>
           <input className='input-cadastrologin' type="text" placeholder='E-mail' />
           <label className='lbl-inputs'>Senha</label>
           <input className='input-cadastrologin' type="password" placeholder='Senha' />
+          {!isLogin && <label className='lbl-inputs'>Confirmar Senha</label>}
+          {!isLogin && <input className='input-cadastrologin' type="text" placeholder='Confirmar Senha' />}
         </div>
       </div>
 
@@ -24,3 +33,4 @@ function CaixaCadastroLogin() {
 }
 
 export default CaixaCadastroLogin
+
