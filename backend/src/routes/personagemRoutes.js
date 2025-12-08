abfb18a4f874e4641e6a0c6dedaf5933d0321590
cassianobigodudo <kakacassiano2010@hotmail.com>
2025-12-08 15:54:37 -0300

    criando a parte de MeusPersonagens, com backend e banco de dados

--- backend/src/routes/personagemRoutes.js
+++ backend/src/routes/personagemRoutes.js
@@ -7,5 +7,6 @@ const personagemController = require('../controllers/personagemController');
 router.post('/', personagemController.criarPersonagem);
 router.post('/gerar-historia', personagemController.gerarHistoria);
 router.get('/buscar-loja', personagemController.buscarPersonagensLoja);
+router.get('/usuario/:id_usuario', personagemController.listarPorUsuario);
 
 module.exports = router;
